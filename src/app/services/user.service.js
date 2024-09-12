e940769329a012fab58e617214a9575373f3fc04
Achonam <162897305+Achonam@users.noreply.github.com>
2024-09-12 08:01:14 +0100

    Update user.service.js

--- src/app/services/user.service.js
+++ src/app/services/user.service.js
@@ -1,32 +1,105 @@
-import { ConflictError } from "../../lib/error-definitions.js";
-import { User } from "../schema/user.schema.js";
+import {User} from "../models/user.model.js";
+import { ConflictError, NotFoundError, UnauthenticatedError, UnauthorizedError } from "../../lib/error-definitions.js";
 
-export const createUser = async (payload) => {
-  // check if a record already exists with the user details
-  const user = await User.findOne({
-    $or: [{ email: payload.email }, { username: payload.username }],
-  });
 
-  // if the user exists, throw an error that the user record exists
-  if (user)
-    throw new ConflictError("A user with the provided details already exists");
+ export class UserService {
+  async createUser(data) {
+    try {
+      const user = await User.create(data);
+      return user;
+    } catch (error) {
+      throw new UnauthenticatedError("Error creating user: " + error.message);
+    }
+  }
 
-  // create a new user record
-  return await User.create(payload);
-};
+  async getUserById(id) {
+    try {
+      const user = await User.findByPk(id);
+      if (!user) {
+        throw new NotFoundError("User not found");
+      }
+      return user;
+    } catch (error) {
+      throw new ConflictError ("Error fetching user: " + error.message);
+    }
+  }
 
-export const getUser = async (id) => {
-  return await User.findById(id);
-};
+  async getAllUsers() {
+    try {
+      const users = await User.findAll();
+      return users;
+    } catch (error) {
+      throw new UnauthorizedError ("Error fetching users: " + error.message);
+    }
+  }
 
-export const getUserByEmail = async (email) => {
-  return await User.findOne({ email });
-};
+  async updateUser(id, data) {
+    try {
+      const user = await User.findByPk(id);
+      if (!user) {
+        throw new NotFoundError ("User not found");
+      }
+      await user.update(data);
+      return user;
+    } catch (error) {
+      throw new ConflictError ("Error updating user: " + error.message);
+    }
+  }
 
-export const getUserByRole = async (role) => {
-  return await User.find({ role });
-};
+  async deleteUser(id) {
+    try {
+      const user = await User.findByPk(id);
+      if (!user) {
+        throw new NotFoundError("User not found");
+      }
+      await user.destroy();
+      return { message: "User deleted successfully" };
+    } catch (error) {
+      throw new ConflictError("User can not be deleted: " + error.message);
+    }
+  }
+}
 
-export const deleteUsers = async (role) => {
-  await User.deleteMany(role);
-};
+
+
+
+
+
+
+
+
+
+
+
+// import { ConflictError } from "../../lib/error-definitions.js";
+// import { User } from "../schema/user.schema.js";
+
+// export const createUser = async (payload) => {
+//   // check if a record already exists with the user details
+//   const user = await User.findOne({
+//     $or: [{ email: payload.email }, { username: payload.username }],
+//   });
+
+//   // if the user exists, throw an error that the user record exists
+//   if (user)
+//     throw new ConflictError("A user with the provided details already exists");
+
+//   // create a new user record
+//   return await User.create(payload);
+// };
+
+// export const getUser = async (id) => {
+//   return await User.findById(id);
+// };
+
+// export const getUserByEmail = async (email) => {
+//   return await User.findOne({ email });
+// };
+
+// export const getUserByRole = async (role) => {
+//   return await User.find({ role });
+// };
+
+// export const deleteUsers = async (role) => {
+//   await User.deleteMany(role);
+// };
